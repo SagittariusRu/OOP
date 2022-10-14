fb56d4b349e9068569c79228940689acccf4536c
Sagittarius <playermiwa7000@gmail.com>
2022-10-14 22:10:19 +0300

    add task 1

--- module_1/week_4/task_1/lab_1_4_1.java
+++ module_1/week_4/task_1/lab_1_4_1.java
@@ -1,7 +1,40 @@
 package module_1.week_4.task_1;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class lab_1_4_1 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.println("Введите путь к файлу: ");
+        String file = scanner.nextLine();
+        System.out.println(getLines(file));
+    }
+
+    public static String getLines(String pathName) {
+        BufferedReader bufferedReader;
+
+        try {
+            bufferedReader = new BufferedReader(new FileReader(pathName));
+        } catch (FileNotFoundException e) {
+            System.out.println("Файл не найден");
+            throw new RuntimeException(e);
+        }
+
+        StringBuilder stringBuilder = new StringBuilder();
+        String readLine;
+        while (true) {
+            try {
+                if ((readLine = bufferedReader.readLine()) == null) break;
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            stringBuilder.append(readLine).append("\n");
+        }
+        return stringBuilder.toString();
     }
 }
--- module_1/week_4/task_2/lab_1_4_2.java
+++ module_1/week_4/task_2/lab_1_4_2.java
@@ -1,7 +1,9 @@
 package module_1.week_4.task_2;
 
+import java.util.Scanner;
+
 public class lab_1_4_2 {
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
     }
 }
