747af0fb3f96f479484bed61e1f2347347018576
Sagittarius <playermiwa7000@gmail.com>
2022-10-15 01:01:34 +0300

    add task 2

--- module_1/week_4/task_2/lab_1_4_2.java
+++ module_1/week_4/task_2/lab_1_4_2.java
@@ -1,6 +1,5 @@
 package module_1.week_4.task_2;
 
-import java.util.Objects;
 import java.util.Scanner;
 
 public class lab_1_4_2 {
@@ -12,9 +11,69 @@ public class lab_1_4_2 {
 
         System.out.println("");
         String type = scanner.nextLine();
+
+        System.out.printf("%s: %s -> %s: %s",
+                message,
+                message.getClass().getSimpleName(),
+                calculation(message, type),
+                calculation(message, type).getClass().getSimpleName()
+        );
     }
 
-    public static Objects calculation(String message, String type) {
-
+    public static Object calculation(String message, String type) {
+        switch (type) {
+            case "Integer" -> {
+                try {
+                    return Integer.valueOf(message);
+                } catch (NumberFormatException e) {
+                    throw new RuntimeException(e);
+                }
+            } case "Float" -> {
+                try {
+                    return Float.valueOf(message);
+                } catch (NumberFormatException e) {
+                    throw new RuntimeException(e);
+                }
+            } case "Double" -> {
+                try {
+                    return Double.valueOf(message);
+                } catch (NumberFormatException e) {
+                    throw new RuntimeException(e);
+                }
+            } case "Character" -> {
+                try {
+                    return new char[message.length()];
+                } catch (NumberFormatException e) {
+                    throw new RuntimeException(e);
+                }
+            } case "Byte" -> {
+                try {
+                    return Byte.valueOf(message);
+                } catch (NumberFormatException e) {
+                    throw new RuntimeException(e);
+                }
+            } case "Short" -> {
+                try {
+                    return Short.valueOf(message);
+                } catch (NumberFormatException e) {
+                    throw new RuntimeException(e);
+                }
+            } case "Long" -> {
+                try {
+                    return Long.valueOf(message);
+                } catch (NumberFormatException e) {
+                    throw new RuntimeException(e);
+                }
+            } case "Boolean" -> {
+                try {
+                    return Boolean.valueOf(message);
+                } catch (NumberFormatException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            default -> {
+                return message;
+            }
+        }
     }
 }
--- module_1/week_4/task_3/lab_1_4_3.java
+++ module_1/week_4/task_3/lab_1_4_3.java
@@ -1,4 +1,9 @@
 package module_1.week_4.task_3;
 
 public class lab_1_4_3 {
+    public class IncorrectFileNameException extends Exception {
+        public IncorrectFileNameException(String errorMessage) {
+            super(errorMessage);
+        }
+    }
 }
