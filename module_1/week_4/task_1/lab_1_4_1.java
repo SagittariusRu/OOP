071e4674cbf69770b7d7f3de4f859fba8934964e
Sagittarius <playermiwa7000@gmail.com>
2022-10-14 12:20:05 +0300

    add lab_1_4_1/2

---
+++ module_1/week_4/task_1/lab_1_4_1.java
@@ -0,0 +1,7 @@
+package module_1.week_4.task_1;
+
+public class lab_1_4_1 {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ module_1/week_4/task_2/lab_1_4_2.java
@@ -0,0 +1,7 @@
+package module_1.week_4.task_2;
+
+public class lab_1_4_2 {
+    public static void main(String[] args) {
+
+    }
+}
