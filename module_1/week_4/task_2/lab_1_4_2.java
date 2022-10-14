769d39c0263eea6a6a839ce6024372a23fd23c0d
Sagittarius <playermiwa7000@gmail.com>
2022-10-15 01:00:55 +0300

    add task 3

--- module_1/week_4/task_2/lab_1_4_2.java
+++ module_1/week_4/task_2/lab_1_4_2.java
@@ -1,9 +1,20 @@
 package module_1.week_4.task_2;
 
+import java.util.Objects;
 import java.util.Scanner;
 
 public class lab_1_4_2 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
+        System.out.println("");
+        String message = scanner.nextLine();
+
+        System.out.println("");
+        String type = scanner.nextLine();
+    }
+
+    public static Objects calculation(String message, String type) {
+
     }
 }
---
+++ module_1/week_4/task_3/lab_1_4_3.java
@@ -0,0 +1,4 @@
+package module_1.week_4.task_3;
+
+public class lab_1_4_3 {
+}
